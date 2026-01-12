b0a82efb308c11a6131799bbf4974d742182d20f
Au-venturous-Buddy <auventurous.buddy@gmail.com>
2026-01-12 16:22:57 +0800

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.b733.pal.gfea.auvb',
+  appName: "Au-venturous Buddy Gotta Fly 'Em All: Philippine Airlines B737-300",
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/assets/index.md
@@ -0,0 +1,5 @@
+---
+title: "Philippine Airlines B737-300"
+retired: true
+category: "Wordpress Posts"
+---
